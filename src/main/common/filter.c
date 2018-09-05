b287bfbe19802340196410ebaa4ee2ac6c15201d
Konstantin Sharlaimov (DigitalEntity) <konstantin.sharlaimov@gmail.com>
2018-09-05 19:21:20 +0200

    Fix non-working AGL estimation [ci skip]

--- src/main/common/filter.c
+++ src/main/common/filter.c
@@ -40,11 +40,12 @@ float nullFilterApply(void *filter, float input)
 // f_cut = cutoff frequency
 void pt1FilterInitRC(pt1Filter_t *filter, float tau, float dT)
 {
+    filter->state = 0.0f;
     filter->RC = tau;
     filter->dT = dT;
 }
 
-void pt1FilterInit(pt1Filter_t *filter, uint8_t f_cut, float dT)
+void pt1FilterInit(pt1Filter_t *filter, float f_cut, float dT)
 {
     pt1FilterInitRC(filter, 1.0f / (2.0f * M_PIf * f_cut), dT);
 }
