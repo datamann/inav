f5ec6bddc6eb17e6c3ea701f429bea1db20d31b3
Konstantin Sharlaimov (DigitalEntity) <konstantin.sharlaimov@gmail.com>
2018-11-01 23:40:24 +0100

    Refactor pitot tube sensor; Change filtering and defaults; Change I2C driver code to handle 0xFF register address; Better MS4525 sensor

--- src/main/target/REVO/target.h
+++ src/main/target/REVO/target.h
@@ -24,8 +24,8 @@
 #define USBD_SERIALNUMBER_STRING "0x8020000"
 #endif
 
-// Use target-specific hardware descriptors (don't use common_hardware.h)
-#define USE_TARGET_HARDWARE_DESCRIPTORS
+// Use target-specific MAG hardware descriptors (don't use common_hardware.h)
+#define USE_TARGET_MAG_HARDWARE_DESCRIPTORS
 
 #define LED0                    PB5
 #define LED1                    PB4
@@ -66,8 +66,9 @@
 #define BARO_I2C_BUS            BUS_I2C1
 #define USE_BARO_MS5611
 
-//#define USE_PITOT_MS4525
-//#define PITOT_I2C_BUS           BUS_I2C2
+#define USE_PITOT
+#define PITOT_I2C_BUS           BUS_I2C2
+#define USE_PITOT_MS4525
 
 #define USE_OPTICAL_FLOW
 #define USE_OPFLOW_CXOF
